1354448c4b5663fda123cf4dad0939a381c6eaa1
PKier <PKier@DESKTOP-B9LTLRV>
2022-01-09 19:43:21 +0100

    Dodaj pliki projektów.

---
+++ KodDaVinci/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KodDaVinci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
